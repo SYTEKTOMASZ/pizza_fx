4e8d5e8b6f7e4e9fbfc96aa8dfefc87ec16dd14c
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-05-24 15:51:17 +0200

    java fx co tu sie dzieje v1000000

--- src/main/java/pizza_fx/Main.java
+++ src/main/java/pizza_fx/Main.java
@@ -10,14 +10,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //FXMLLoader -> załadowanie pliku .fxml i zwrcenie jako obiektu
+        Parent root = FXMLLoader.load(getClass().getResource("/view/pizzaView.fxml"));
+        //Utworzenie obiektu Scene wypełnionego widokiem aplikacji
+        Scene scene = new Scene(root);
+        // Ustawienie obiektu Scene w Stage (oknie aplikacji)
+        primaryStage.setTitle("Pizza");
+        primaryStage.setScene(scene);
+        // Zatrzymanie okna aplikacji
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
--- src/main/java/pizza_fx/controller/PizzaController.java
+++ src/main/java/pizza_fx/controller/PizzaController.java
@@ -1,5 +1,7 @@
 package pizza_fx.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -7,23 +9,38 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import pizza_fx.model.Ingredient;
+import pizza_fx.model.Pizza;
+import pizza_fx.model.PizzaModel;
+import pizza_fx.service.PizzaService;
+
+import javax.swing.*;
+import java.util.stream.Collectors;
 
 public class PizzaController {
+// Aby dodać kolekcje  obiektów do kontroek FXML korzystamy z ObservableList
+    private ObservableList<PizzaModel> pizzas = FXCollections.observableArrayList();
+// Objekt klasy Pizza service
+    private PizzaService pizzaService = new PizzaService();
+
 
     @FXML
-    private TableView<?> tblPizza;
+    private Label lblSum;
 
     @FXML
-    private TableColumn<?, ?> tcName;
+    private TableView<PizzaModel> tblPizza;
 
     @FXML
-    private TableColumn<?, ?> tcIngredients;
+    private TableColumn<PizzaModel, String> tcName;
 
     @FXML
-    private TableColumn<?, ?> tcType;
+    private TableColumn<PizzaModel, String> tcIngredients;
 
     @FXML
-    private TableColumn<?, ?> tcPrice;
+    private TableColumn<PizzaModel, String> tcType;
+
+    @FXML
+    private TableColumn<PizzaModel, Double> tcPrice;
 
     @FXML
     private Label lbRandomPizza;
@@ -39,12 +56,30 @@ public class PizzaController {
 
     @FXML
     void clearAction(MouseEvent event) {
-
+        pizzaService.clearOrder(taBasket, tfAddress, tfPhone, lblSum);
     }
 
     @FXML
     void orderAction(MouseEvent event) {
+        System.out.println("112");
+    }
+
+    @FXML
+    void selectPizzaAction(MouseEvent mouseEvent){
+        //odczyt, który wiersz w tabelce został zaznaczony
+        pizzaService.addToBasket(tblPizza, taBasket);
+        // utworzenie okna kontekstowego do zamowienia wybranej ilosci pizzy
 
     }
+
+    // konstruktor -> incjalizacja
+public void initialize(){
+        // wywołanie metody zaimplementowanych w logice biznesowej aplikacji
+       pizzas =  pizzaService.addPizzas(pizzas);
+       pizzaService.insertPizzasToTable(
+               tblPizza, tcName, tcIngredients, tcType,tcPrice, pizzas);
+       pizzaService.pizzaOfTheDayGenerator(pizzas, lbRandomPizza);
+}
+
 
 }
